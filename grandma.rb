089b798e42e812f65749df178444aea9232e4406
Scott Weinstein <sweinstein27@gmail.com>
2018-04-24 19:09:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(string)
   # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 if string == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN"
+  return "I LOVE YOU TOO PUMPKIN!"
 elsif string == string.upcase
   return "NO, NOT SINCE 1938!"
 else

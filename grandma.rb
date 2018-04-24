ebfa97dc3250069af0d38392715d81e55a59c0f5
Scott Weinstein <sweinstein27@gmail.com>
2018-04-24 19:08:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
 # HUH?! SPEAK UP, SONNY!
 if string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU PUMPKIN"
-elsif string = string.upcase
+elsif string == string.upcase
   return "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
